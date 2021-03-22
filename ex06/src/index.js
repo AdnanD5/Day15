0b203f8941af835b26a266a9a9b36d21abc6320c
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-22 18:15:08 +0100

    Dodavanje ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+//Only change code below this line
+const arena = ()=>{
+    "use strict";
+    return new Date();
+};
+//Only change code below this line
+console.log(arena());
+module.exports = arena;
